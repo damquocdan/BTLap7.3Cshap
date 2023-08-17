5dc97fb95715ebb031f59b15ad7b4ef1af162db8
Đàm Quốc Dân <124369155+damquocdan@users.noreply.github.com>
2023-08-17 19:06:44 +0700

    Add project files.

---
+++ InvalidMarkException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTLap7._3Cshap
+{
+    public class InvalidMarkException : Exception
+    {
+        public InvalidMarkException() : base("Invalid mark value provided.")
+        {
+        }
+
+        public InvalidMarkException(string message) : base(message)
+        {
+        }
+
+        public InvalidMarkException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+
+}
